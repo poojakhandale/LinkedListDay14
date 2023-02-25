b63376ca5538493ed4d1683d944d170ab4ef7f81
poojakhandale <poojakhandale2000@gmail.com>
2023-02-25 23:32:02 +0530

    Appending the value

--- Node.cs
+++ Node.cs
@@ -15,6 +15,34 @@ namespace LinkedList
         {
             this.data = data;
         }
-    }
 
+        public class LinkList
+        {
+
+            public Node head;
+            private int Count;
+
+            public void Add(int data)
+            {
+                Node node = new Node(data);
+                Count++;
+
+                if (head == null)
+                {
+                    head = node;
+                }
+                else
+                {
+                    Node temp = this.head;
+
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} is created", node.data);
+            }
+        }
+    }
 }
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Node head = new Node(56);
+        Node second = new Node(70);
+        Node tail = new Node(30);
+        head.next = second;
+        second.next = tail;
+
+    }
+}
+
+

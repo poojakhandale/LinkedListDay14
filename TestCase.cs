ef77f70291bb810f3036c9b8d4c04ea056d49057
poojakhandale <poojakhandale2000@gmail.com>
2023-02-28 23:50:40 +0530

    test is completed

---
+++ TestCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataStructure.Node;
+
+namespace LinkedList
+{
+ public class TestCase
+    {
+        public void tc8(MyLinkList linkedList, int Search_element, int new_element)
+        {
+            int position = linkedList.Search(30);
+            if (position == 0)
+            {
+                Console.WriteLine("element is not found");
+            }
+            else
+            {
+                linkedList.insert(position + 1, new_element);
+                linkedList.display();
+                Console.WriteLine("element is found in " + position + "and new element " + new_element + " is inserted");
+            }
+        }
+
+    }
+}
+
+    
+

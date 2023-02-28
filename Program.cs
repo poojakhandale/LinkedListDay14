87c9bfa0a9deb9d46288e613da7fd5e95635cb6e
poojakhandale <poojakhandale2000@gmail.com>
2023-02-28 23:50:14 +0530

    test is completed

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LinkedList;
-using static LinkedList.Node;
+using static DataStructure.Node;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-    
-            MyLinkList linkList = new MyLinkList();
-            linkList.Add(56);
-            linkList.Add(70);
-            linkList.insert(2, 30);
-             
-        linkList.Search(30);
-
-        linkList.display();
-        }
+        MyLinkList linkList = new MyLinkList();
+        linkList.Add(56);
+        linkList.Add(70);
+        linkList.insert(2, 30);
+        TestCase mycase = new TestCase();
+        mycase.tc8(linkList, 30, 40);
     }
+}
+
+
+
+
 
 
